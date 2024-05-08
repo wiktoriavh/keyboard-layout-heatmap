ac519b3243ff4a3111ca8a2d1caeb0ff491878c3
Wiktoria Van Harneveldt <w.dev+wslgit@mailbox.org>
2024-05-08 21:55:52 +0200

    refactor: Update defaultHeatmap object with color property

---
+++ src/defaulHeatmap.ts
@@ -0,0 +1,140 @@
+export type Heatmap = {
+  [x: string]: {
+    heat: number;
+    number: number;
+    color: string;
+  };
+};
+
+export const defaultHeatmap: Heatmap = {
+  a: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  b: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  c: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  d: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  e: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  f: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  g: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  h: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  i: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  j: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  k: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  l: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  m: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  n: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  o: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  p: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  q: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  r: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  s: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  t: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  u: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  v: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  w: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  x: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  y: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+  z: {
+    heat: 0,
+    number: 0,
+    color: "",
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,149 +1,9 @@
+import { Heatmap, defaultHeatmap } from "./defaulHeatmap";
 import "./style.css";
 
 import Alpine from "alpinejs";
 
 window.Alpine ??= Alpine;
-
-type Heatmap = {
-  [x: string]: {
-    heat: number;
-    number: number;
-    color: string;
-  };
-};
-
-const defaultHeatmap: Heatmap = {
-  a: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  b: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  c: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  d: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  e: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  f: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  g: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  h: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  i: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  j: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  k: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  l: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  m: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  n: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  o: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  p: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  q: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  r: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  s: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  t: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  u: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  v: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  w: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  x: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  y: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-  z: {
-    heat: 0,
-    number: 0,
-    color: "",
-  },
-};
 
 function readTextFile(file: string): Promise<string> {
   return fetch(file)
@@ -156,7 +16,7 @@ function readTextFile(file: string): Promise<string> {
 
 Alpine.data("keyboard", () => ({
   layout: "",
-  sample: "uiaeosnrtdyuaonty",
+  sample: "",
   heatmap: defaultHeatmap,
   samples: {} as Record<string, string>,
   layouts: {} as Record<string, string>,
