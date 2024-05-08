814a676f8f33d6e9b0e7a09b5a58f52a99dcdc3e
Wiktoria Van Harneveldt <w.dev+wslgit@mailbox.org>
2024-05-08 17:51:02 +0200

    fix: combine both vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss()],
-})
+// export default defineConfig({
+//   plugins: [tailwindcss()],
+// })
+
+export default defineConfig(({ command }) => {
+  if (command === "build") {
+    return {
+      base: "/keyboard-layout-heatmap/",
+      plugins: [tailwindcss()],
+    };
+  }
+
+  return {
+    base: "/",
+    plugins: [tailwindcss()],
+  };
+});
